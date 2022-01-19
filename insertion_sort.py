ff3e5c611aad8d25176778506d00cff01c034137
Julius von Davier <vdavier@protonmail.com>
2022-01-19 19:06:45 +0100

    improved time complexity of time efficiency

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,10 +4,13 @@ def insertion_sort(arr):
 
     for i in range(1, n):
         if arr[i] < arr[i-1]:
-            arr[i], arr[i-1] = arr[i-1], arr[i]
-
             for j in range(i, 0, -1):
                 if arr[j] < arr[j-1]:
                     arr[j], arr[j-1] = arr[j-1], arr[j]
+                else:
+                    break
 
     return arr
+
+
+print(insertion_sort([5, 1, 120, 2, -123]))
